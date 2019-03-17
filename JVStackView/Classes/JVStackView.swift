8394233a049e6d5a4bc58f0b904621e16fbbc302
Jasper <Jasperav@hotmail.com>
2019-03-17 14:31:44 +0100

    commit

--- JVStackView/Classes/JVStackView.swift
+++ JVStackView/Classes/JVStackView.swift
@@ -4,11 +4,15 @@ open class JVStackView: UIStackView {
     
     public static let spacing: CGFloat = 5
     
-    public init(axis: NSLayoutConstraint.Axis, spacing: CGFloat = JVStackView.spacing) {
+    public init(axis: NSLayoutConstraint.Axis, spacing: CGFloat = JVStackView.spacing, subviews: [UIView] = []) {
         super.init(frame: .zero)
         
         self.axis = axis
         self.spacing = spacing
+        
+        for subview in subviews {
+            addArrangedSubview(subview)
+        }
     }
     
     public required init(coder: NSCoder) {
